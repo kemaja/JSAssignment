3aa376c6e966420797ddf4c5677bda9a70f91cf2
Maja Emese Kaleem <151825005+kemaja@users.noreply.github.com>
2025-10-01 10:28:37 +0200

    updated

--- js/app.js
+++ js/app.js
@@ -3,3 +3,33 @@ console.log('Second!');
 console.log('Third!');
 console.log('Fourth!');
 console.log('Fifth!');
+
+
+function greet() {
+  console.log("Hello!");
+}
+// Call the function
+greet();
+
+// This is a single-line comment.
+let x = 5;
+let x1 = 3; // everything after this is
+
+
+/*
+  You will never see
+*/
+
+/*
+ But it is part of the
+ assignment
+*/
+
+
+// This function adds two numbers and prints the result
+function printSum(a, b) {
+  // Add the numbers using the + operator
+  let result = a + b;
+  // Print the result
+  console.log("Sum is:", result);
+}
